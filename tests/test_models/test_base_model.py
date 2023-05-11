0fdcd6ed55757a3a841642286cb32492ae9108e1
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-05-11 23:28:12 +0300

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,3 +2,28 @@
 """
 unittests for base_model.py.
 """
+
+from datetime import datetime
+from models.base_model import BaseModel
+import unittest
+
+class TestBaseModel(unittest.TestCase):
+    """
+    Test Cases for the BaseModel Class.
+    """
+
+    def setUp(self):
+        """
+        Instantiate an object before running each test.
+        """
+        self.model1 = BaseModel()
+
+    def tearDown(self):
+        """
+        Tears down an object after running each test.
+        """
+        del self.model1
+
+
+if __name__ == "__main__":
+    unittest.main() """
