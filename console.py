7e3140ec58d1ff92a2873e1ace723b488845d751
FatimaEzzatMhran <f.ezzat94@gmail.com>
2023-05-15 00:02:10 +0300

    console.py

--- console.py
+++ console.py
@@ -207,7 +207,8 @@ class HBNBCommand(cmd.Cmd):
             obj = objects_dict["{}.{}".format(argl[0], argl[1])]
             for key, value in eval(argl[2]).items():
                 if (key in obj.__class__.__dict__.keys() and
-                        type(obj.__class__.__dict__[key]) in {str, int, float}):
+                        type(obj.__class__.__dict__[key]) in
+                        {str, int, float}):
                     value_type = type(obj.__class__.__dict__[key])
                     obj.__dict__[key] = value_type(value)
                 else:
